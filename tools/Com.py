05be0d2488ccb9ef7dc118fbafda28a65318a803
huangyf530 <lancerken530@outlook.com>
2018-12-03 15:38:24 +0800

    保存之前工作

--- tools/Com.py
+++ tools/Com.py
@@ -1,5 +1,8 @@
 import serial
 import struct
+import re
+import socket
+import sys
 
 def int_to_byte_string(val):
     return struct.pack('<I', val)
@@ -7,11 +10,88 @@ def int_to_byte_string(val):
 def byte_string_to_int(val):
 	return struct.unpack('<I', val)[0]
 
+class tcp_wrapper:
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def write(self, msg):
+        totalsent = 0
+        MSGLEN = len(msg)
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def flush(self): # dummy
+        pass
+
+    def read(self, MSGLEN):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            # print 'read:...', list(map(lambda c: hex(ord(c)), chunk))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+    def reset_input_buffer(self):
+        local_input = [self.sock]
+        while True:
+            inputReady, o, e = select.select(local_input, [], [], 0.0)
+            if len(inputReady) == 0:
+                break
+            for s in inputReady:
+                s.recv(1)
+
+def InitializeTCP(host_port):
+    
+    ValidIpAddressRegex = re.compile("^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])):(\d+)$");
+    ValidHostnameRegex = re.compile("^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])):(\d+)$");
+
+    if ValidIpAddressRegex.search(host_port) is None and \
+        ValidHostnameRegex.search(host_port) is None:
+        return False
+
+    match = ValidIpAddressRegex.search(host_port) or ValidHostnameRegex.search(host_port)
+    groups = match.groups()
+    ser = tcp_wrapper()
+    host, port = groups[0], groups[4]
+    sys.stdout.write("connecting to %s:%s..." % (host, port))
+    sys.stdout.flush()
+    ser.connect(host, int(port))
+    print("connected")
+
+    global outp, inp
+    outp = ser
+    inp = ser
+    return True
+
 if __name__ == "__main__":
-    tty = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
-    tty.reset_input_buffer()
+    # tty = serial.Serial(port="/dev/ttyACM0", baudrate=9600)
+    # tty.reset_input_buffer()
+    if not InitializeTCP("166.111.227.237:42645"):
+        print("Connect to TCP failed!")
+        exit(1)
     while(True):
         # print ("fuck")
         x = input("fuck : ")
-        tty.write(int_to_byte_string(int(x)))
+        a = int_to_byte_string(int(x))
+        outp.write(a)
         print (hex(x))
+        print (byte_string_to_int(a))
+        print(ord(a[0]))
+        print(ord(a[1]))
+        print(ord(a[2]))
+        print(ord(a[3]))
